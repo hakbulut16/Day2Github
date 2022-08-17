509d38ef20e43d6ead22724a9bb966dd93195c2d
Hakan AKBULUT <hakanakbulut016@gmail.com>
2022-08-17 11:51:59 +0300

    3.C 17.08.2022

--- src/main/java/day1Git.java
+++ src/main/java/day1Git.java
@@ -2,5 +2,6 @@ public class day1Git {
     public static void main(String[] args) {
         System.out.println("Hello Git 1. Deneme");
         System.out.println("Git 2.Deneme");
+        System.out.println("Git 3. Deneme");
     }
 }
